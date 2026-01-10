b62d70430dd961cae308aa865e7e313c99939593
Leigh MacDonald <leigh.macdonald@gmail.com>
2026-01-09 18:52:47 -0700

    return 0

--- services/binary-cache-client.nix
+++ services/binary-cache-client.nix
@@ -7,7 +7,7 @@
               set -f # disable globbing
               export IFS=' '
               echo "Uploading paths" $OUT_PATHS
-              exec nix copy --to "https://cache.roto.lol" $OUT_PATHS"
+              exec nix copy --to "https://cache.roto.lol" $OUT_PATHS || exit 0"
       '';
       mode = "0777";
     };
