32418c25386e87687f1c11d9ccc78f9c3ded4463
Leigh MacDonald <leigh.macdonald@gmail.com>
2026-01-09 18:33:12 -0700

    Add cahces

--- nix.nix
+++ nix.nix
@@ -11,7 +11,7 @@
         "x86_64-linux"
       ];
       substituters = [
-        #"https://cache.roto.lol"
+        "https://cache.roto.lol"
         "https://nix-community.cachix.org"
         "https://cache.nixos.org"
         "https://hyprland.cachix.org"
@@ -21,7 +21,7 @@
         "flakes"
       ];
       trusted-public-keys = [
-        #"cache.roto.lol:QN6aF1eT7KvC/lFzLCikuChntrRAEAanO5d91IbTLnY="
+        "cache.roto.lol:QN6aF1eT7KvC/lFzLCikuChntrRAEAanO5d91IbTLnY="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
