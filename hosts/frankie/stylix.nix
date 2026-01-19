83459c1440fa1d6e3dddcfc2b9d4b0b862844713
Leigh MacDonald <leigh.macdonald@gmail.com>
2026-01-18 18:41:37 -0700

    New theme

--- hosts/frankie/stylix.nix
+++ hosts/frankie/stylix.nix
@@ -7,10 +7,10 @@
   imports = [ inputs.stylix.nixosModules.stylix ];
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/equilibrium-dark.yaml";
-    #base16Scheme = "${pkgs.base16-schemes}/share/themes/phd.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/chalk.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/embers.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/phd.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-hard.yaml";
-    #
     fonts = {
       sizes = {
         applications = 16;
