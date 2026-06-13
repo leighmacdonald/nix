7b748edcfe8ef3a96554bbcbee78f168d0afe3ba
Leigh MacDonald <leigh.macdonald@gmail.com>
2026-06-13 02:28:53 -0600

    Use number for mpd port

--- services/mpd.nix
+++ services/mpd.nix
@@ -28,7 +28,7 @@ in
       user = username;
       settings = {
         bind_to_address = "${listenAddress}";
-        port = "${toString 6600}";
+        port = 6600;
         music_directory = "${musicDirectory}";
         sticker_file = "/storage/music/.sticker.sql";
         save_absolute_paths_in_playlists = "yes";
